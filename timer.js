77aa739b22c2d8cd8edb1aa0e855b3094716c1cf
Vasy228123007 <60486871+Vasy228123007@users.noreply.github.com>
2020-06-12 10:39:14 +0300

    CSS
    1. Added CSS
    2. Added new bugs
    3. Connected the watch

--- timer.js
+++ timer.js
@@ -1,28 +1,36 @@
 var rezyltat = document.getElementById("timer");
-//rezyltat.innerText = "Проверка";
 
 var c = document.getElementById("chas"); // Час
 var m = document.getElementById("min"); // Минуты
 var s = document.getElementById("sekyn"); // Секунды
 var k = document.getElementById("knopka"); // Кнопка
 
+rezyltat.innerText = "00:00:00";
+
 k.onclick = function(e) {
     //alert('Таймер успешно запущен'); //Уведомления о запуске таймера
-    var minyt = m.value;
-    var sekyn = s.value;
+    var minyt = m.value ? m.value : 0;
+    var sekyn = s.value ? s.value : 0;
+    var houri = c.value ? c.value : 0;
 
     var interval = setInterval(function() {
-        rezyltat.innerText = minyt+":"+sekyn;
+        rezyltat.innerText = houri+":"+minyt+":"+sekyn;
         sekyn = sekyn -1;
-        if(minyt<=0){
-            minyt=0
+        if(houri<=0){
+            houri=0
         }
-        if(sekyn<=0){
+        if(minyt<=0){
+            houri = houri -1
+            minyt=59
+        }
+        if(sekyn<=-1){
             minyt=minyt-1;
             sekyn=59;
             if(minyt<=0){
-                alert('Таймер завершон');
-                clearInterval(interval);
+                if(houri<=0){
+                    alert('Таймер завершон');
+                    clearInterval(interval);
+                }
             }
         }
     }, 1000)
