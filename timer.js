13ed8f583f676f7f47ac18edb7bef26e187d7119
Vasy228123007 <60486871+Vasy228123007@users.noreply.github.com>
2020-06-15 00:57:56 +0300

    Обновление до v1.3
    1.Подключили библиотеку JS
    2.Добавили проверку на текст
    3.Добавили новую кнопку "Сбросить"
    4.Добавил теперь ставиться по умолчянию 0
    5.Добавил новую кнопку (нужно доработать)
    6.Добавили анимацию при остановте таймера
    7.Добавили звук при остановке таймера
    =========================
    Планы
    1.Подключить кнопку (будет ли играть музыка при остановке)
    2.Изменить дизайн таймера
    3.Добавить фон
    4.Добавить выбор анимаций при остановке таймера
    5.Добавить выбор звука при остановке таймера
    
    На этом думаю все.

--- timer.js
+++ timer.js
@@ -1,37 +1,85 @@
-var rezyltat = document.getElementById("timer");
+{
+    var rezyltat = document.getElementById("timer");
+    var interval;
+    var c = document.getElementById("chas"); // Час
+    var m = document.getElementById("min"); // Минуты
+    var s = document.getElementById("sekyn"); // Секунды
+    var k = document.getElementById("knopka"); // Кнопка Start
+    var ks = document.getElementById("knopkastop") // Кнопка Stop
+    var i = document.getElementById("img"); // Звездочка
+    var error = document.getElementById("error"); // Ошибки
+    var audio = new Audio('stop.mp3');    // Музыка
+    var music = document.getElementById("music");   // Музыку [Вкл/Выкл]
+    var audio = {};
+}
+    rezyltat.innerText = "00:00:00";
+    function startAudio(url) {
+        if("pause" in audio) audio.pause();
+      
+        audio = new Audio(url);
+        audio.play();
+      }
 
-var c = document.getElementById("chas"); // Час
-var m = document.getElementById("min"); // Минуты
-var s = document.getElementById("sekyn"); // Секунды
-var k = document.getElementById("knopka"); // Кнопка
-
-rezyltat.innerText = "00:00:00";
-
+$("#knopkastop").attr("disabled", true);
 k.onclick = function(e) {
-    //alert('Таймер успешно запущен'); //Уведомления о запуске таймера
+    clearInterval(interval);
     var minyt = m.value ? m.value : 0;
     var sekyn = s.value ? s.value : 0;
     var houri = c.value ? c.value : 0;
 
-    var interval = setInterval(function() {
-        rezyltat.innerText = houri+":"+minyt+":"+sekyn;
+    if(!c.value){
+        c.style="border:1px solid red";
+        $("#error").fadeIn();
+        error.innerText = "Вы не ввели часы";
+        return;
+    }
+    if(!m.value){
+        m.style="border:1px solid red";
+        $("#error").fadeIn();
+        error.innerText = "Вы не ввели минуты";
+        return;
+    }
+    if(!s.value){
+        s.style="border:1px solid red";
+        $("#error").fadeIn();
+        error.innerText = "Вы не ввели секунды";
+        return;
+    }
+    $("#knopka").attr("disabled", true);
+    $("#knopkastop").attr("disabled", false);
+    interval = setInterval(function() 
+    {
+        var houri_txt = (houri<10) ? "0"+houri : houri;
+        var minyt_txt = (minyt<10) ? "0"+minyt : minyt;
+        var sekyn_txt = (sekyn<10) ? "0"+sekyn : sekyn;
+        rezyltat.innerText = houri_txt+":"+minyt_txt+":"+sekyn_txt;
         sekyn = sekyn -1;
-        if(houri<=0){
-            houri=0
-        }
-        if(minyt<=0){
-            houri = houri -1
-            minyt=59
-        }
         if(sekyn<=-1){
             minyt=minyt-1;
             sekyn=59;
             if(minyt<=0){
+                houri = houri -1
+                minyt=59
                 if(houri<=0){
-                    alert('Таймер завершон');
+                    houri=0
+                    alert('Таймер оставновлен');
                     clearInterval(interval);
+                    $( "#timer" ).fadeOut().delay(100);
+                    $( "#timer" ).fadeIn();
+                if(music<=0)
+                    audio.play();
                 }
+                $("#knopka").attr("disabled", false);
             }
         }
     }, 1000)
+}
+i.onclick = function(e) {
+    alert('Нажмите CTRl-D, чтобы добавить страницу в закладки.');
+}
+ks.onclick = function(e) {
+    clearInterval(interval);
+    rezyltat.innerText = "00:00:00";
+    alert('Таймер успешно сбросен.');
+    $("#knopka").attr("disabled", false);
 }
